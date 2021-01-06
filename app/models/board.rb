80655fe02fad0624baa16f1f8c60d078f0698c40
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2021-01-06 18:15:40 +0100

    board methods

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,57 @@
 class Board < ApplicationRecord
   belongs_to :game
   has_many :moves
+
+  def initialize
+    @board = Board.new(self)
+    @game = Game.new
+    @board.template = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @board.counter = 0
+    @board.user_id = current_user
+    @board.status = 'pending'
+  end
+
+  def valid_move?
+    @move = Move.new
+    @board.template[@move.position - 1] == ' '
+  end
+
+  def move
+    @move = Move.new
+    if valid_move?
+      if @board.user_id == current_user
+        @board.template[@move.position - 1] = 'X'
+      else
+        @board.template[@move.position - 1] = 'O'
+      end
+    end
+    @board.counter += 1
+  end
+
+  def turn
+    if @board.counter % 2 == true
+      @board.user_id = 2
+    else
+      @board.user_id = current_user
+    end
+  end
+
+  def won?
+    @board.game.win += 1
+    @board.status = 'finished'
+    true
+  end
+
+  def lost?
+    @board.game.lost += 1
+    @board.status = 'finished'
+    true
+  end
+
+  def tie?
+    if !won? || !lost? && @board.counter == 9
+      @board.status = 'finished'
+      true
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ puts "starting seed"
 
 User.destroy_all
 
-charles = User.create(email: "ch.boyeldieu@gmail.com", password: "123456")
+charles = User.create(email: "charles@gmail.com", password: "123456")
+computer = User.create(email: "computer@gmail.com", password: "123456")
 
 puts "seed finished"
